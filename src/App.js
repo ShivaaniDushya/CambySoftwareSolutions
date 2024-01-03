49adddaa40341f8f3857df57039dd7da32cfa827
ShivaaniDushya <it20006884@my.sliit.lk>
2024-01-03 10:59:35 +0530

    App JS Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
-// import ContactUs from "./components/contact/ContactUs";
+import ContactUs from "./components/contact/ContactUs";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -11,7 +11,7 @@ function App() {
       <ScrollToTop/>
       <Routes>
         <Route exact path="/" element={<Home/>} />
-        {/* <Route exact path="/contactUs" element={<ContactUs/>} /> */}
+        <Route exact path="/contactUs" element={<ContactUs/>} />
       </Routes>
       <Footer/>
     </Router>
